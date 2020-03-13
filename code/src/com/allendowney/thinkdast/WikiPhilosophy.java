6188c760f838566fc7495bb2e121fa65d760f41a
a.osetckiy <a.osetckiy@extracode.net>
2020-03-13 15:09:05 +0300

    Fixed_Exercise_5

--- code/src/com/allendowney/thinkdast/WikiPhilosophy.java
+++ code/src/com/allendowney/thinkdast/WikiPhilosophy.java
@@ -39,15 +39,32 @@ public class WikiPhilosophy {
      * @param source
      * @throws IOException
      */
-    public static boolean testConjecture(String destination, String source, int limit) throws IOException {
+    public static void testConjecture(String destination, String source, int limit) throws IOException {
         // Right now, this method tries the first link on the page, and if it is the destination, it returns true
         // TODO: fix this method.
         // Loop until reach limit, get stuck in a loop, reach a page with no links, or reach the destination
-        Element elt = getFirstValidLink(source);
-        String url = elt.attr("abs:href");
-        if (url.equals(destination))
-            return true;
-        return false;
+        String url = source;
+        for (int i=0; i<limit; i++) {
+            if (visited.contains(url)) {
+                System.err.println("We're in a loop, exiting.");
+                return;
+            } else {
+                visited.add(url);
+            }
+            Element elt = getFirstValidLink(url);
+            if (elt == null) {
+                System.err.println("Got to a page with no valid links.");
+                return;
+            }
+
+            System.out.println("**" + elt.text() + "**");
+            url = elt.attr("abs:href");
+
+            if (url.equals(destination)) {
+                System.out.println("Eureka!");
+                break;
+            }
+        }
     }
 
     /**
